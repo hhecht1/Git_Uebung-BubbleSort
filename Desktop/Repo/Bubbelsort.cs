358dcafea64b483371f784d6feeb3f1b2c555630
Helge <helgehecht92@gmail.com>
2025-09-04 14:47:35 +0200

    Feat : Buuble sort Algorithmus

---
+++ Desktop/Repo/Bubbelsort.cs
@@ -0,0 +1,39 @@
+using System;
+
+public class BubbleSort
+{
+    public static void Main(string[] args)
+    {
+        int[] zahlen = { 5, 1, 4, 2, 8 };
+        Console.WriteLine("Unsortierte Zahlen:");
+        DruckeArray(zahlen);
+
+        Sortiere(zahlen);
+
+        Console.WriteLine("\nSortierte Zahlen:");
+        DruckeArray(zahlen);
+    }
+
+    public static void Sortiere(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            // Die innere Schleife durchläuft das Array
+            for (int j = 0; j < n - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void DruckeArray(int[] arr)
+    {
+        Console.WriteLine(string.Join(", ", arr));
+    }
+}
